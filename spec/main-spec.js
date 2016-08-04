ae951b887f1fc18e56b32cd240409d7875f572f8
xiayu0321 <xiayugogo0321@163.com>
2016-08-04 14:31:17 +0800

    second exer

---
+++ spec/main-spec.js
@@ -0,0 +1,103 @@
+let {generateTodoHtml} = require('../src/main.js');
+
+describe('string-test3', () => {
+
+  fit('generate todo html with some items selected', () => {
+    let todoItems = [
+      {name: '吃饭', completed: true},
+      {name: '练习', completed: false},
+      {name: '锻炼', completed: true},
+      {name: '睡觉', completed: false}
+    ];
+    let result = generateTodoHtml(todoItems);
+    let expected = `<section>
+    <header>
+        <h1>todos</h1>
+        <form><input type="text" autofocus="" placeholder="What needs to be done?"/></form>
+    </header>
+    <section>
+        <input type="checkbox"/>
+        <label for="toggle-all">Mark all as complete</label>
+        <ul>
+            <li>
+                <div><input type="checkbox" checked="checked"/><label>吃饭</label></div>
+                <form><input type="text"/></form>
+            </li>
+            <li>
+                <div><input type="checkbox"/><label>练习</label></div>
+                <form><input type="text"/></form>
+            </li>
+            <li>
+                <div><input type="checkbox" checked="checked"/><label>锻炼</label></div>
+                <form><input type="text"/></form>
+            </li>
+            <li>
+                <div><input type="checkbox"/><label>睡觉</label></div>
+                <form><input type="text"/></form>
+            </li>
+        </ul>
+    </section>
+    <footer>
+        <strong>2</strong> items left
+        <ul>
+            <li><a href="#/">All</a></li>
+            <li><a href="#/active">Active</a></li>
+            <li><a href="#/completed">Completed</a></li>
+        </ul>
+        <button>Clear completed</button>
+    </footer>
+</section>`
+    require('fs').writeFileSync('./4.txt',expected)
+    expect(result).toEqual(expected);
+  });
+
+  fit('generate todo html with no item selected', ()=> {
+    let todoItems = [
+      {name: '吃饭', completed: false},
+      {name: '练习', completed: false},
+      {name: '锻炼', completed: false},
+      {name: '睡觉', completed: false}
+    ];
+    let result = generateTodoHtml(todoItems);
+    let expected = `<section>
+    <header>
+        <h1>todos</h1>
+        <form><input type="text" autofocus="" placeholder="What needs to be done?"/></form>
+    </header>
+    <section>
+        <input type="checkbox"/>
+        <label for="toggle-all">Mark all as complete</label>
+        <ul>
+            <li>
+                <div><input type="checkbox"/><label>吃饭</label></div>
+                <form><input type="text"/></form>
+            </li>
+            <li>
+                <div><input type="checkbox"/><label>练习</label></div>
+                <form><input type="text"/></form>
+            </li>
+            <li>
+                <div><input type="checkbox"/><label>锻炼</label></div>
+                <form><input type="text"/></form>
+            </li>
+            <li>
+                <div><input type="checkbox"/><label>睡觉</label></div>
+                <form><input type="text"/></form>
+            </li>
+        </ul>
+    </section>
+    <footer>
+        <strong>4</strong> items left
+        <ul>
+            <li><a href="#/">All</a></li>
+            <li><a href="#/active">Active</a></li>
+            <li><a href="#/completed">Completed</a></li>
+        </ul>
+        <button>Clear completed</button>
+    </footer>
+</section>`;
+    require('fs').writeFileSync('./5.txt',expected)
+    expect(result).toEqual(expected);
+  });
+
+});
---
+++ src/main.js
@@ -0,0 +1,48 @@
+function generateTodoHtml(todoItems) {
+    let num = 0;
+    let lines = [];
+lines.push(`<section>`);
+lines.push(`    <header>`);
+lines.push(`        <h1>todos</h1>`);
+lines.push(`        <form><input type="text" autofocus="" placeholder="What needs to be done?"/></form>`);
+lines.push(`    </header>`);
+lines.push(`    <section>`);
+lines.push(`        <input type="checkbox"/>`);
+lines.push(`        <label for="toggle-all">Mark all as complete</label>`);
+lines.push(`        <ul>`);
+    for(let todoItem of todoItems) {
+        if(todoItem.completed === true) {
+            lines.push( `            <li>
+                <div><input type="checkbox" checked="checked"/><label>${todoItem.name}</label></div>
+                <form><input type="text"/></form>
+            </li>`);
+        }else {
+            num++;
+            lines.push(`            <li>
+                <div><input type="checkbox"/><label>${todoItem.name}</label></div>
+                <form><input type="text"/></form>
+            </li>`);
+        }
+    }
+lines.push(`        </ul>`);
+lines.push(`    </section>`);
+lines.push(`    <footer>`);
+lines.push(`        <strong>${num}</strong> items left`);
+lines.push(`        <ul>`);
+lines.push(`            <li><a href="#/">All</a></li>`);
+lines.push(`            <li><a href="#/active">Active</a></li>`);
+lines.push(`            <li><a href="#/completed">Completed</a></li>`);
+lines.push(`        </ul>`);
+lines.push(`        <button>Clear completed</button>`);
+lines.push(`    </footer>`);
+lines.push(`</section>`);
+
+    let lineString = lines.join('\n');
+
+require('fs').writeFileSync('./6.txt',lineString);
+  return lineString;
+}
+
+module.exports = {
+  generateTodoHtml:generateTodoHtml
+}
