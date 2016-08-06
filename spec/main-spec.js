7cfb9e7ddd46e0b14acaa454f75aa098a2132285
xiayu0321 <xiayugogo0321@163.com>
2016-08-06 15:12:01 +0800

    exer

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -10,8 +10,7 @@ describe('string-test3', () => {
       {name: '睡觉', completed: false}
     ];
     let result = generateTodoHtml(todoItems);
-    let expected = `
-<section>
+    let expected = `<section>
     <header>
         <h1>todos</h1>
         <form><input type="text" autofocus="" placeholder="What needs to be done?"/></form>
@@ -49,10 +48,11 @@ describe('string-test3', () => {
     </footer>
 </section>
 `
+    require('fs').writeFileSync('./2.txt',expected);
     expect(result).toEqual(expected);
   });
 
-  it('generate todo html with no item selected', ()=> {
+  fit('generate todo html with no item selected', ()=> {
     let todoItems = [
       {name: '吃饭', completed: false},
       {name: '练习', completed: false},
@@ -60,8 +60,7 @@ describe('string-test3', () => {
       {name: '睡觉', completed: false}
     ];
     let result = generateTodoHtml(todoItems);
-    let expected = `
-<section>
+    let expected = `<section>
     <header>
         <h1>todos</h1>
         <form><input type="text" autofocus="" placeholder="What needs to be done?"/></form>
@@ -99,6 +98,7 @@ describe('string-test3', () => {
     </footer>
 </section>
 `;
+    require('fs').writeFileSync('./3.txt',expected);
     expect(result).toEqual(expected);
   });
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,45 @@
 function generateTodoHtml(todoItems) {
-  return /**/;
+  let lines = [];
+  let num = 0;
+
+lines.push(`<section>`);
+lines.push(`    <header>`);
+lines.push(`        <h1>todos</h1>`);
+lines.push(`        <form><input type="text" autofocus="" placeholder="What needs to be done?"/></form>`);
+lines.push(`    </header>`);
+lines.push(`    <section>`);
+lines.push(`        <input type="checkbox"/>`);
+lines.push(`        <label for="toggle-all">Mark all as complete</label>`);
+lines.push(`        <ul>`);
+  for(let todoItem of todoItems){
+    if(todoItem.completed === true){
+      lines.push(`            <li>`);
+      lines.push(`                <div><input type="checkbox" checked="checked"/><label>${todoItem.name}</label></div>`);
+      lines.push(`                <form><input type="text"/></form>`);
+      lines.push(`            </li>`);
+    }else{
+      num++;
+      lines.push(`            <li>`);
+      lines.push(`                <div><input type="checkbox"/><label>${todoItem.name}</label></div>`);
+      lines.push(`                <form><input type="text"/></form>`);
+      lines.push(`            </li>`);
+    }
+  }
+lines.push(`        </ul>`);
+lines.push(`    </section>`);
+lines.push(`    <footer>`);
+lines.push(`        <strong>${num}</strong> items left`);
+lines.push(`        <ul>`);
+lines.push(`            <li><a href="#/">All</a></li>`);
+lines.push(`            <li><a href="#/active">Active</a></li>`);
+lines.push(`            <li><a href="#/completed">Completed</a></li>`);
+lines.push(`        </ul>`);
+lines.push(`        <button>Clear completed</button>`);
+lines.push(`    </footer>`);
+lines.push(`</section>\n`);
+  let str = lines.join('\n');
+  require('fs').writeFileSync('./1.txt',str);
+  return str;
 }
 
 module.exports = {
